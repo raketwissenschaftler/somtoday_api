e9d48257aae5d4b4b6f8ad4ed70763bb6d2a9c18
raketwissenschaftler <ricktrein@hotmail.com>
2014-11-26 15:02:35 +0100

    Thanks to stephan for the php part

---
+++ somtoday.php
@@ -0,0 +1,191 @@
+<?php
+
+/**
+ * @author Stephan Meijer <me@stephanmeijer.com>
+ * @copyright MIT (09/11/2014)
+ * @version 1.0.0
+ */
+
+class SOMtodayUser {
+
+	public $pupilID;
+
+	public $fullname;
+
+	public $username;
+
+	private $password;
+
+	public $schoolName;
+
+	/**
+	 * BRIN-code
+	 * 
+	 * @link http://nl.wikipedia.org/wiki/Basis_Registratie_Instellingen Wikpedia page for more info
+	 */
+	public $BRIN;
+
+	/**
+	 * Base url, as a base for requests
+	 */
+	public $baseURL;
+
+	/**
+	 * @param String $username User's SOMtoday username
+	 * @param String $password User's SOMtoday password
+	 * @param String $schoolName The name of your school, as found on http://servers.somtoday.nl
+	 * @param String $BRIN The BRIN-code of your school, an unique identifier
+	 */
+	function __construct($username, $password, $schoolName, $BRIN) {
+
+		$this->username = $username;
+		$this->schoolName = $schoolName;
+		$this->BRIN = $BRIN;
+
+		$login = $this->login($username, $password);
+
+	}
+
+	/**
+	 * SOMtoday's way of hashing password is NOT SECURE!
+	 * 
+	 * @param String $password Plain-text password
+	 * @return String Hashed and encoded password
+	 */
+	private function hashAndEncodePassword($password) {
+
+		// Yes, SOMtoday is using SHA1. This is a shame!
+		$hash = sha1( $password );
+
+		// Base64, how useless..
+		$hash = base64_encode( $hash );
+
+		// Converting string to hex, another useless layer
+		$hexPassword = "";
+		for ($i=0; $i < strlen( $password ); $i++) {
+			$hexPassword .= dechex( ord( $password[ $i ] ) );
+		}
+
+		$hash = $hexPassword;
+
+	}
+
+	/**
+	 * 
+	 */
+	private function login($username, $password) {
+
+		$passwordHash = $this->hashAndEncodePassword( $password );
+
+		$baseURL = "https://somtoday.nl/" . $this->schoolName . "/services/mobile/v10/";
+
+		$url = $baseURL . "Login/CheckMultiLoginB64/" . $username . "/" . $password . "/" . $this->BRIN;
+
+		$response = json_decode( file_get_contents( $login_url ) );
+
+		if ( $response["error"] == "SUCCESS" ) {
+
+			$fullName = $response["leerlingen"][0]["fullName"];
+			$pupilID = (string) $response["leerlingen"][0]["leerlingId"];
+
+			$this->fullName = $fullName;
+			$this->baseURL = $baseURL;
+			$this->pupilID = $pupilID;
+			$this->password = $passwordHash;
+
+			return array(
+				"success" => true,
+				"fullname" => $fullName,
+				"pupil_id" => $pupilID
+			);
+
+		} elseif ( $response["error"] == "FEATURE_NOT_ACTIVATED" ) {
+
+			return array(
+				"success" => false,
+				"error_message" => "This school doesn't support the SOMtoday API."
+			);
+
+		} elseif ( $response["error"] == "FAILED_AUTHENTICATION" ) {
+
+			return array(
+				"success" => false,
+				"error_message" => "Invalid login details."
+			);
+
+		} elseif ( $response["error"] == "FAILED_OTHER_TYPE" ) {
+
+			return array(
+				"success" => false,
+				"error_message" => "This account isn't supported."
+			);
+
+		} else {
+
+			return array(
+				"success" => false,
+				"error_message" => "Unknown error."
+			);
+
+		}
+
+	}
+
+	/**
+	 * @return Array An array with the most recent grades, ordered by age
+	 */
+	public function getGrades() {
+		$url = $this->baseURL . "Cijfers/GetMultiCijfersRecentB64/" . $this->username . "/" . $this->password . "/" . $this->BRIN . "/" . $this->pupilID;
+        $response = json_decode( file_get_contents( $url ) );
+
+        return $response["data"];
+	}
+
+	/**
+	 * @param Int $daysAhead A number that indicates the amount of days ahead the homework has to be displayed
+	 * @return Array An array with all homework until the amount of days ahead
+	 */
+	public function getHomework($daysAhead = 14) {
+
+		$daysAhead = (string) $daysAhead;
+
+        $url = $this->baseURL . "Agenda/GetMultiStudentAgendaHuiswerkMetMaxB64/" . $this->username . "/" . $this->password . "/" . $this->BRIN . "/" . $daysAhead . "/" . $this->pupilID;
+
+        $response = json_decode( file_get_contents( $url ) );
+
+        return $response["data"];
+	}
+
+	/**
+	 * @param Int $daysAhead A number that indicates the amount of days ahead it has to fetch the schedule for. Default=0
+	 * @return Array All classes for the current day, or a specified amount of days ahead.
+	 */
+	public function getSchedule($daysAhead = 0) {
+		$date = ( time() + ( $daysAhead * 86400 ) ) * 1000;
+
+        $url = $this->baseURL . "Agenda/GetMultiStudentAgendaB64/" . $this->username . "/" . $this->password . "/" . $this->BRIN . "/" . (string) $date . "/" . $this->pupilID;
+        $response = json_decode( file_get_contents( $url ) );
+
+        return $response["data"];
+	}
+
+	/**
+	 * Changes the status of the given homework.
+	 * 
+	 * @param Int $homeworkID The "huiswerkid" returned by getHomework()
+	 * @param Int $appointmentID Yhe "afspraakid" returneb by getHomework()
+	 * @param Boolean $status The status you want to give that assignment. True is done, false is not done.
+	 * 
+	 * @return Boolean True if the status change succeeded, false if not.
+	 */
+    public function changeHomeworkStatus($homeworkID, $appointmentID, $status) {
+
+        $url = $this->baseURL . "Agenda/HuiswerkDoneB64/" . $this->username . "/" . $this->password . "/" . $this->BRIN . "/" . (string) $appointmentID. "/" . (string) $homeworkID . "/" . (string) $status;
+        
+        $responseStatus = json_decode( file_get_contents( $url ) )["status"];
+        
+        return ( $responseStatus == "OK" );
+
+    }
+
+}
